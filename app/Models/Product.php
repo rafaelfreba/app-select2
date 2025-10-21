137e49ced658764bb3ce858fc3e78af9d023273d
J-J-T-M <jonatham.junior.18@gmail.com>
2025-10-21 13:06:15 -0400

    feat: Atualiza o método getSelectList da classe Product para incluir o parâmetro dependValue

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 use App\Contracts\HasSelect2List;
 use App\Http\Resources\Select2Resource;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class Product extends Model implements HasSelect2List
 {
     use HasFactory;
-    public static function getSelectList(?string $search = null): AnonymousResourceCollection
+    public static function getSelectList(?string $search = null, ?string $dependValue = null): AnonymousResourceCollection
     {
         $query = static::query();
 
+        if ($dependValue) {
+            $query->where('category_id', $dependValue);
+        }
+        
         if ($search) {
             $query->where('name', 'like', "%{$search}%");
         }
