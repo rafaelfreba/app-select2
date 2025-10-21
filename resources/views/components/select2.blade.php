bcaefab25feef8acef0b710044706b008a118f62
J-J-T-M <jonatham.junior.18@gmail.com>
2025-10-21 13:07:20 -0400

    feat: Melhora a inicialização do Select2 e força a exibição do valor selecionado inicial

--- resources/views/components/select2.blade.php
+++ resources/views/components/select2.blade.php
@@ -1,10 +1,13 @@
 <div>
+    <label for="{{ $name }}">{{ $name }}</label>
     <select
         id="{{ $name }}"
         name="{{ $name . ($multiple ? '[]' : '') }}"
         class="form-control select2-{{ str_replace(['[', ']'], '', $name) }} {{ $validClass }}"
         @if ($multiple) multiple="multiple" @endif
         style="width: 100%;"
+        data-dependent="{{ $dependent }}"
+        data-value="{{ $dependentValue }}"
     >
         @foreach ($selectedOptions as $option)
             <option value="{{ $option['id'] }}" selected>
@@ -21,46 +24,91 @@
 </div>
 
 @push('scripts')
-    <script>
-        $(document).ready(function () {
-            $('.select2-{{ $name }}').select2({
-                placeholder: "{{ $placeholder }}",
-                allowClear: true,
-                theme: 'bootstrap4',
-                language: 'pt-BR',
-                ajax: {
-                    url: "{{ route('select2.list', ['model' => $model]) }}",
-                    dataType: 'json',
-                    delay: 250,
-                    data: function (params) {
-                        return {
-                            search: params.term || '',
-                            page: params.page
-                        };
-                    },
-                    processResults: function (data, params) {
-                        params.page = params.page || 1;
+<script>
+$(document).ready(function () {
 
-                        if (data.data && Array.isArray(data.data)) {
-                            return {
-                                results: data.data.map(function (item) {
+    let $selectElement = $('.select2-{{ str_replace(['[', ']'], '', $name) }}');
+    let dependentOn = $selectElement.data('dependent');
+    let currentCascadeValue = $selectElement.data('value');
+    let baseUrl = "{{ route('select2.list', ['model' => $model]) }}";
 
-                                    return {
-                                        id: item.id,
-                                        text: item.name // Usa 'name' do objeto retornado
-                                    };
-                                }),
-                                pagination: {
-                                    more: data.meta.current_page < data.meta.last_page
-                                }
-                            };
-                        }
+    function initializeSelect2(cascadeValue) {
+        currentCascadeValue = cascadeValue;
 
-                        return { results: [] };
-                    },
-                    cache: true
+        if ($selectElement.data('select2')) {
+            $selectElement.select2('destroy');
+        }
+
+        $selectElement.select2({
+            placeholder: "{{ $placeholder }}",
+            allowClear: true,
+            theme: 'bootstrap4',
+            language: 'pt-BR',
+            ajax: {
+                url: function () {
+                    return currentCascadeValue
+                        ? baseUrl + '/' + currentCascadeValue
+                        : baseUrl;
                 },
-            });
+                dataType: 'json',
+                delay: 250,
+                data: function (params) {
+                    return {
+                        search: params.term || '',
+                        page: params.page
+                    };
+                },
+                processResults: function (data, params) {
+                    params.page = params.page || 1;
+
+                    if (data.data && Array.isArray(data.data)) {
+                        return {
+                            results: data.data.map(function (item) {
+                                return {
+                                    id: item.id,
+                                    text: item.name
+                                };
+                            }),
+                            pagination: {
+                                more: data.meta.current_page < data.meta.last_page
+                            }
+                        };
+                    }
+
+                    return { results: [] };
+                },
+                cache: true
+            }
         });
-    </script>
+    }
+
+    if (dependentOn) {
+        let $dependentElement = $('#' + dependentOn);
+
+        $dependentElement.on('select2:select', function (e) {
+            let newCascadeValue = e.params.data.id;
+            $selectElement.empty().trigger('change');
+            initializeSelect2(newCascadeValue);
+        });
+
+        $dependentElement.on('select2:unselect', function () {
+            $selectElement.empty().trigger('change');
+            initializeSelect2(null);
+        });
+    }
+
+    // ⚡️ ADIÇÃO: Força exibir o valor selecionado inicial (pai, ex: category_id)
+    @if (!empty($selectedOptions))
+        @foreach ($selectedOptions as $option)
+            if (!$selectElement.find("option[value='{{ $option['id'] }}']").length) {
+                let newOption = new Option("{{ $option['text'] }}", "{{ $option['id'] }}", true, true);
+                $selectElement.append(newOption).trigger('change');
+            }
+        @endforeach
+    @endif
+
+    initializeSelect2(currentCascadeValue);
+});
+</script>
 @endpush
+
